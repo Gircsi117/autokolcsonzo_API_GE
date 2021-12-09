6de0d188921a5de8690d0dc4148f728b0ebce093
Gircsi117 <gilianerik1107@gmail.com>
2021-12-09 11:14:56 +0100

    Regisztráció működik

--- API/Controllers/userControler.js
+++ API/Controllers/userControler.js
@@ -26,17 +26,34 @@ exports.login = (req, res)=>{
 }
 
 exports.reg = (req, res)=>{
-    const data = {"name": req.body.name, "pass":req.body.pass, "email":req.body.email}
-    pool.query(`INSERT INTO felhasznalo VALUES (null, '${data.name}', '${data.email}', SHA1('${data.pass}'))`, (err)=>{
-        if (err) {
-            console.log(err);
-            res.status(500).json({"message": err.message})
-        }
-        else{
-            res.status(200).json(data)
-        }
-    })
-    
+    const data = {"name": req.body.name, "pass":req.body.pass, "email":req.body.email, "key":req.body.key}
+
+    if (data.key == "ZsalmA") {
+        pool.query(`SELECT * FROM felhasznalo WHERE email = '${data.email}' OR jelszo = SHA1('${data.pass}')`, (err1, data1)=>{
+            if (err1) {
+                res.status(500).json({message: false, err:"Adatbázis hiba!"})
+            }
+            else{
+                if (data1.length == 0) {
+                    pool.query(`INSERT INTO felhasznalo VALUES (null, '${data.name}', '${data.email}', SHA1('${data.pass}'))`, (err)=>{
+                        if (err) {
+                            console.log(err);
+                            res.status(500).json({message: err.message, err: "Adatbázis hiba!"});
+                        }
+                        else{
+                            res.status(200).json({message: true});
+                        }
+                    })
+                }
+                else{
+                    res.status(200).json({message: false, err:"Email vagy jelszó már használatban van!"})
+                }
+            }
+        })
+    }
+    else{
+        res.status(200).json({message: false, err:"Hibás kulcs"})
+    }
 }
 
 exports.logout = (req, res)=>{
--- Frontend/js/code.js
+++ Frontend/js/code.js
@@ -13,11 +13,11 @@ catch (error) {
     console.log(error);
 }
 
-console.log(bente, "igen");
+console.log(bente);
 
 if (!bente && !String(document.location.href).includes("login")) {
     console.log("Alma");
-    document.location.href = "./login.html"
+    document.location.href = "./login.html";
 }
 
 //Login
@@ -49,11 +49,58 @@ try {
             }
             else{
                 console.log("Sikertelen");
-                document.getElementById("errorMSG-login").innerHTML = "Hibás felhasználói adatok!"
+                $("#errorMSG-login").html("Hibás felhasználói adatok!")
             }
     
         }
     }
+} catch (error) {}
+
+//registration
+try {
+    
+    document.getElementById("regForm").onsubmit = async (event)=>{
+        event.preventDefault();
+        
+        const name = $("#regName").val();
+        const email = $("#regEmail").val();
+        const pass1 = $("#regPassword1").val();
+        const pass2 = $("#regPassword2").val();
+        const key = $("#key").val();
+
+        $("#errorMSG-reg").html("");
+        if (pass1 != pass2) {
+            $("#errorMSG-reg").html("A jelszavak nem egyeznek!");
+        }
+        else{
+            const response = await fetch("http://localhost:3000/reg", {
+                method: "POST",
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    name: name,
+                    email: email,
+                    pass: pass1,
+                    key: key
+                })
+            });
+
+            if (response.status == 200) {
+                const data = await response.json();
+                console.log(data);
+                if (data.message == true) {
+                    alert("Sikeres regisztráció!")
+                    class_valt_form('regForm', 'loginForm', 'd-none');
+                }
+                else{
+                    $("#errorMSG-reg").html(data.err);
+                }
+            }
+        }
+
+    }
+
 } catch (error) {}
 
 //logout
@@ -65,5 +112,7 @@ async function log_out(params) {
     }
 }
 
-function classValt() {
+function class_valt_form(idadd, idremove, clas) {
+    document.getElementById(idremove).classList.remove(clas);
+    document.getElementById(idadd).classList.add(clas);
 }
